e7902efa11cf48ee15041213a272c64f429b1e9e
JD10S <dodevp10@gmail.com>
2025-12-19 12:11:17 -0500

    fix(db): add imageurl column to products table and map in entity

--- Marketplace.Entities/Entities/Product.cs
+++ Marketplace.Entities/Entities/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Threading.Tasks;
 
 namespace Marketplace.Entities.Entities
@@ -13,6 +14,7 @@ namespace Marketplace.Entities.Entities
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; } 
         public int Stock { get; set; } 
+        [Column("imageurl")]
         public string ImageUrl { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } 
 
